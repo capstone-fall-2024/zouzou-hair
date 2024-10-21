167e4dc53a6e2527877f00c1c03a4e2e66faf839
Ryan Morrow <122489904+rmorrow4-git@users.noreply.github.com>
2024-10-21 15:47:09 -0600

    files prepped for about us page

--- page-about.php
+++ page-about.php
@@ -12,6 +12,12 @@ get_header();
 
 ?>
 <main id="primary" class="site-main">
+<div class="about-hero-banner">
+    <img src="../assets/images/joseph.webp" alt="Master barber Joseph Hayek">
+</div>
+<div>
+<p>We believe that great hair is the foundation of confidence. Our team of expert stylists delivers personalized, high-quality services ensuring you leave feeling your absolute best.</p>    
+</div>
 
 </main><!-- #main -->
 
