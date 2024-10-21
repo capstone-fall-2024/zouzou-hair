931102809f63e4b4c19b8b558149b92354536315
Eunice <90015739+xxceanne@users.noreply.github.com>
2024-10-21 15:49:53 -0600

    test commit

--- page-services.php
+++ page-services.php
@@ -10,12 +10,26 @@
 
 get_header();
 
+$args = array(
+    'post_type'      => 'services',
+    'posts_per_page' => -1,
+    'tax_query'      => array(
+        array(
+            'taxonomy' => 'service-category',
+            'field' => 'slug',
+            'terms'    => $service->slug,
+        ),
+    ),
+);
+$service_posts = new WP_Query($args);
+
 ?>
 <main id="primary" class="site-main">
 
     <div class="container padding">
 
         <div class="intro-text">
+            <p>Test</p>
             <p>At Zouzou Hair, we offer a variety of services to make sure you get the exact look you want.<br>Browse our services and previous works down below!</p>
         </div>
 
@@ -30,30 +44,17 @@ get_header();
 
             foreach ($services as $service) : 
                 $service_link = get_term_link($service);
-
             ?>
                 <div class="service">
                     <h2><a href="<?php echo esc_url($service_link); ?>"><?php echo esc_html($service->name) ?></a></h2>
-                </div>
+
                 <?php
-                $args = array(
-                    'post_type'      => 'services',
-                    'posts_per_page' => -1,
-                    'tax_query'      => array(
-                        array(
-                            'taxonomy' => 'service-category',
-                            'field' => 'slug',
-                            'terms'    => $service->slug,
-                        ),
-                    ),
-                );
-                $service_posts = new WP_Query($args);
                 if ($service_posts->have_posts()) : ?>
                     <div class="services-list">
                         <?php
                         while ($service_posts->have_posts()) :
                             $service_posts->the_post(); ?>
-                            <div>
+                            <div class="service-bar">
                                 <div>
                                     <p><?php the_title(); ?></p>
                                     <p><?php echo esc_html(get_field('duration')); ?></p>
@@ -61,12 +62,14 @@ get_header();
                                 <p><?php echo esc_html(get_field('price')); ?></p>
                             </div>
                         <?php endwhile; ?>
-                    </div>
+                    </div> <!-- end of service-list div -->
+                </div> <!-- end of service div -->
         <?php
                 endif;
                 wp_reset_postdata();
+
             endforeach; // end of foreach loop
-        } // end of if condition
+        } // end of service if condition
         ?>
     </div>
 
