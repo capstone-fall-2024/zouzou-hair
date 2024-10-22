9cc587118eb2b250aa64823732e144163350fd7c
Eunice <90015739+xxceanne@users.noreply.github.com>
2024-10-21 19:39:36 -0600

    updated service pages to display images

--- page-services.php
+++ page-services.php
@@ -10,19 +10,6 @@
 
 get_header();
 
-$args = array(
-    'post_type'      => 'services',
-    'posts_per_page' => -1,
-    'tax_query'      => array(
-        array(
-            'taxonomy' => 'service-category',
-            'field' => 'slug',
-            'terms'    => $service->slug,
-        ),
-    ),
-);
-$service_posts = new WP_Query($args);
-
 ?>
 <main id="primary" class="site-main">
 
@@ -37,37 +24,69 @@ $service_posts = new WP_Query($args);
         <?php
         $services = get_terms(array(
             'taxonomy'   => 'service-category',
-            'hide_empty' => false
+            'hide_empty' => false,
+            'orderby'    => 'term_id',
+            'order'      => 'ASC'
         ));
+
         if (!empty($services) && !is_wp_error($services)) {
-
             foreach ($services as $service) : 
-                $service_link = get_term_link($service);
-            ?>
-                <div class="service">
+                $service_link = get_term_link($service);?>
+                <div class="service-category">
                     <h2><a href="<?php echo esc_url($service_link); ?>"><?php echo esc_html($service->name) ?></a></h2>
-                    
-                <?php
-                if ($service_posts->have_posts()) : ?>
-                    <div class="services-list">
-                        <?php
-                        while ($service_posts->have_posts()) :
-                            $service_posts->the_post(); ?>
-                            <div class="service-bar">
-                                <div>
-                                    <p><?php the_title(); ?></p>
-                                    <p><?php echo esc_html(get_field('duration')); ?></p>
-                                </div>
-                                <p><?php echo esc_html(get_field('price')); ?></p>
-                            </div>
-                        <?php endwhile; ?>
-                    </div> <!-- end of service-list div -->
-                </div> <!-- end of service div -->
-        <?php
-                endif;
-                wp_reset_postdata();
 
-            endforeach; // end of foreach loop
+                    <?php
+
+                    $args = array(
+                        'post_type'      => 'services',
+                        'posts_per_page' => -1,
+                        'tax_query'      => array(
+                            array(
+                                'taxonomy' => 'service-category',
+                                'field' => 'slug',
+                                'terms'    => $service->slug,
+                            ),
+                        ),
+                    );
+                    $service_posts = new WP_Query($args);
+
+                    if ($service_posts->have_posts()) : ?>
+                        <div class="services-list">
+                            <?php while ($service_posts->have_posts()) :
+                                $service_posts->the_post(); ?>
+                                <div class="service-bar">
+                                    <div>
+                                        <p><?php the_title(); ?></p>
+                                        <p><?php echo esc_html(get_field('duration')); ?></p>
+                                    </div>
+                                    <p><?php echo esc_html(get_field('price')); ?></p>
+                                </div>
+                            <?php endwhile; ?>
+                        </div> <!-- end of service-list div -->
+                    <?php endif;
+
+                    wp_reset_postdata();
+
+                    ?>
+                    <div class="gallery">
+
+                        <?php
+                        $images = get_field('images', $service);
+
+                        if (!empty($images)) {
+                            foreach ($images as $image) {
+                                if (!empty($image)) { 
+                                    ?>
+                                    <img src="<?php echo $image['url']; ?>" alt="">
+                        <?php }
+                            }
+                        } ?>
+                    </div> <!-- end of gallery div -->
+
+                    <a href="<?php echo esc_url($service_link); ?>" class="button">View Our Previous Work</a>
+
+                </div> <!-- end of service div -->
+        <?php endforeach;
         } // end of service if condition
         ?>
     </div>
